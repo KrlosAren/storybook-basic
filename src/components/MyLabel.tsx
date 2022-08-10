ab96e81e73f01c171413a0220180dfe959a9dea4
Carlos <krlosaren@gmail.com>
2022-08-10 08:05:58 -0400

    storybook start project

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,44 @@
+import './myLabel.css';
+
+export interface MyLabelProps {
+  /**
+   * Is this the text in the label
+   */
+  label: string;
+  /**
+   * Is this the size of the text label
+   */
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * Capitalize the label text
+   */
+  allCaps?: boolean;
+  /**
+   * Color of the label text
+   */
+  color?: 'primary' | 'secondary' | 'tertiary';
+  /**
+   * Personalize color of text
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  label = 'No label',
+  size = 'normal',
+  allCaps = false,
+  color = 'primary',
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <span
+      style={{
+        textTransform: allCaps ? 'uppercase' : 'none',
+        color: fontColor,
+      }}
+      className={`label ${size} text-${color}`}
+    >
+      {label}
+    </span>
+  );
+};
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,56 @@
+import { MyLabel } from '../../components/MyLabel';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'UI/MyLabel',
+  component: MyLabel,
+  argTypes: {
+    size: {control: 'select',options: ['normal','h1','h2','h3']},
+    color: {control: 'select',options: ['primary','secondary','tertiary']},
+    fontColor: {control: 'color'},
+  }
+} as ComponentMeta<typeof MyLabel>;
+
+const Template: ComponentStory<typeof MyLabel> = (args) => (
+  <MyLabel {...args} />
+);
+
+export const Basic = Template.bind({});
+Basic.args = {
+  label: 'MyLabel',
+  size: 'normal',
+  allCaps: false,
+  color: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'MyLabel',
+  size: 'h2',
+  allCaps: false,
+  color: 'secondary',
+};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+  label: 'MyLabel',
+  size: 'h3',
+  allCaps: false,
+  color: 'tertiary',
+};
+
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+  label: 'MyLabel',
+  allCaps: true,
+};
+
+
+// CUSTOM FONT COLOR
+export const CustomFontColor = Template.bind({});
+CustomFontColor.args = {
+  label: 'MyLabel',
+  size: 'h1',
+  allCaps: false,
+  fontColor: '#ff0000',
+};
